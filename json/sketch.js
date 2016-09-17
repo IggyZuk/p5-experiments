e166bc0b24923924d36ad31e3a66387594851afe
IggyZuk <IgnatusZuk@gmail.com>
2016-09-18 00:05:00 +0100

    Added giphy API.

--- json/sketch.js
+++ json/sketch.js
@@ -4,20 +4,35 @@ var fruits;
 function setup()
 {
 	noCanvas();
+
 	loadJSON("https://raw.githubusercontent.com/dariusk/corpora/master/data/animals/common.json", gotAnimalData);
 	loadJSON("https://raw.githubusercontent.com/dariusk/corpora/master/data/foods/fruits.json", gotFruitData);
+
+	var api = "http://api.giphy.com/v1/gifs/search"
+	var search = "harry potter";
+	var query = "q=" + search + "&limit=100&rating=g&offset=0";
+	var apiKey = "dc6zaTOxFJmzC";
+
+	var url = api + "?" + query + "&api_key=" + apiKey;
+	var data = loadJSON(url, data =>
+	{
+		for (var i = 0; i < data.data.length; i++)
+		{
+			createImg(data.data[i].images.fixed_height.url);
+		}
+	});
 }
 
 function gotAnimalData(data)
 {
 	animals = data.animals;
-	tryShowP();
+	//tryShowP();
 }
 
 function gotFruitData(data)
 {
 	fruits = data.fruits;
-	tryShowP();
+	//tryShowP();
 }
 
 function tryShowP()
