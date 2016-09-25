67d2f3969568c9fc006d257d03a23e0dd398ee02
IggyZuk <IgnatusZuk@gmail.com>
2016-09-25 16:50:53 +0100

    Added noise.

--- sphere/sketch.js
+++ sphere/sketch.js
@@ -1,13 +1,14 @@
 var vertices = [];
 var indices = [];
+
 var drawVertices = false;
+var showColor = true;
 
 function setup() {
-	pixelDensity(0.5);
-	createCanvas(800, 600, WEBGL);
+	pixelDensity(1);
+	createCanvas(1080, 720, WEBGL);
 	colorMode(HSB, 255, 255, 255);
-
-	createSphere(250, 16, 16);
+	createSphere(255, 32, 32);
 }
 
 function createSphere(r, t, p) {
@@ -74,11 +75,37 @@ function Spherical(r, theta, phi) {
 }
 
 function draw() {
-	background(64);
+	background(0);
 	orbitControl();
 
+	var noiseVertices = [];
+	var noiseStrength = 0.0025;
+	var noiseSpeed = 0.01;
+
+	vertices.forEach(v => {
+
+		var noisePoint = new Point3D();
+		noisePoint.x = v.x * noiseStrength + frameCount * noiseSpeed;
+		noisePoint.y = v.y * noiseStrength + frameCount * noiseSpeed;
+		noisePoint.z = v.z * noiseStrength + frameCount * noiseSpeed;
+
+		var noiseValue = map(noise(noisePoint.x, noisePoint.y, noisePoint.z), 0, 1, -0.6, 2.15);
+
+		var vp = new Point3D(v.x, v.y, v.z);
+		var p = new Point3D(
+			vp.x *= noiseValue,
+			vp.y *= noiseValue,
+			vp.z *= noiseValue
+		);
+
+		p.clr = color(map(noiseValue, -0.5, 1.5, 100, 0), 255, 255);
+
+		noiseVertices.push(p);
+	});
+
+
 	if (drawVertices) {
-		vertices.forEach(v => {
+		noiseVertices.forEach(v => {
 			push();
 			basicMaterial(v.clr);
 			translate(
@@ -92,14 +119,23 @@ function draw() {
 	}
 
 	for (var i = 0; i < indices.length - 3; i += 3) {
-		var a = vertices[indices[i]];
-		var b = vertices[indices[i + 1]];
-		var c = vertices[indices[i + 2]];
+		var a = noiseVertices[indices[i]];
+		var b = noiseVertices[indices[i + 1]];
+		var c = noiseVertices[indices[i + 2]];
 
-		fill((hue(a.clr) + hue(b.clr) + hue(c.clr)) / 3, 255, 255);
+		var averageHue = (hue(a.clr) + hue(b.clr) + hue(c.clr)) / 3;
+		if (showColor) fill(averageHue, 255, 255);
+		else fill(averageHue);
+
 		triangle(
 			a.x, a.y, a.z,
 			b.x, b.y, b.z,
-			c.x, c.y, c.z);
+			c.x, c.y, c.z
+		);
+
+		//fill(0);
+		//line(a.x, a.y, a.z, b.x, b.y, b.z);
+		//line(a.x, a.y, a.z, c.x, c.y, c.z);
+		//line(c.x, c.y, c.z, b.x, b.y, b.z);
 	}
 }
