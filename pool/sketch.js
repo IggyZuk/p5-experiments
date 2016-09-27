2d93789d24befb292c140161ddf6ca3ef4fb985b
IggyZuk <IgnatusZuk@gmail.com>
2016-09-27 22:43:19 +0100

    Added space debris.

--- pool/sketch.js
+++ pool/sketch.js
@@ -1,42 +1,31 @@
 var balls = [];
+var slider = null;
 
 function setup() {
 	createCanvas(600, 400);
 	translate(width / 2, height / 2);
+	background(0);
+	frameRate(30);
 
-	for (var i = 0; i < 256; i++) {
+	for (var i = 0; i < 50; i++) {
 		balls.push(
 			new Ball(
 				random(-width / 2, width / 2),
 				random(-height / 2, height / 2),
-				random(2, 10))
+				random(1, 5))
 		);
 	}
+
+	slider = createSlider(0.1, 16, 1);
 }
 
 function draw() {
 	colorMode(RGB);
 	background(0);
 
-	balls.forEach(b => {
-		b.update();
-	});
-
-	balls.forEach(b => {
-		b.collide(balls);
-		b.show();
-	});
-
-	var pb = balls[0];
-	strokeWeight(4);
-	stroke(255, 64);
-	//line(pb.x, pb.y, mouseX - width / 2, mouseY - height / 2);
-}
-
-function mousePressed() {
-	var pb = balls[0];
-	pb.vx = -(pb.x - (mouseX - width / 2)) / 50;
-	pb.vy = -(pb.y - (mouseY - height / 2)) / 50;
+	balls.forEach(b => { b.update(); });
+	balls.forEach(b => { b.collide(balls); });
+	balls.forEach(b => { b.show(); });
 }
 
 function Ball(x, y, r) {
@@ -45,21 +34,44 @@ function Ball(x, y, r) {
 	this.vx = 0;
 	this.vy = 0;
 	this.r = r;
+	this.life = random(1024);
+	this.ax = random(-1, 1);
+	this.ay = random(-1, 1);
 
 	this.brightness = 64;
 
 	this.update = function() {
-		var f = 0.1;
-		var a = 2;
-		this.vx += cos(this.x * f) * a;
-		this.vy += sin(this.y * f) * a;
+		var timeScale = slider.value();
+
+		var s = 0.01;
+		this.vx += -this.x * s;
+		this.vy += -this.y * s;
+
+		this.life += 0.1;
+		this.vx += cos(this.life) * this.ax;
+		this.vy += sin(this.life) * this.ay;
 
 		this.x += this.vx;
 		this.y += this.vy;
 
-		var friction = 0.98;
+		var friction = 0.9;
 		this.vx *= friction;
 		this.vy *= friction;
+
+		if (this.x < -width / 2) {
+			this.x = -width / 2;
+			this.vx = -this.vx;
+		} else if (this.x > width / 2) {
+			this.x = width / 2;
+			this.vx = -this.vx;
+		}
+		if (this.y < -height / 2) {
+			this.y = -height / 2;
+			this.vy = -this.vy;
+		} else if (this.y > height / 2) {
+			this.y = height / 2;
+			this.vy = -this.vy;
+		}
 	}
 
 	this.show = function() {
@@ -82,8 +94,24 @@ function Ball(x, y, r) {
 				var dist = b2.sub(b1);
 				var dir = dist.copy().normalize();
 				var r = ((this.r + b.r) / 2);
+				var mag = dist.mag();
 
-				if (dist.mag() < r - 0.1) {
+				if (mag < 200) {
+					var grav = 0.01;
+					this.vx += dir.x * dist.x * grav;
+					this.vy += dir.y * dist.y * grav;
+					b.vx += -dir.x * dist.x * grav;
+					b.vy += -dir.y * dist.y * grav;
+				}
+
+				if (mag <= 17) {
+					colorMode(RGB);
+					strokeWeight(2);
+					stroke(32);
+					line(this.x, this.y, b.x, b.y);
+				}
+
+				if (mag < r - 0.1) {
 					var tx = this.x;
 					var ty = this.y;
 					var tvx = this.vx;
@@ -95,8 +123,9 @@ function Ball(x, y, r) {
 					b.y = ty + dir.y * r;
 
 					colorMode(HSB);
+					strokeWeight(4);
 					stroke(0, 255, 255);
-					line(this.x, this.y, this.x + dist.x * 3, this.y + dist.y * 3);
+					line(this.x, this.y, b.x + dist.x * 3, this.y + dist.y * 3);
 
 					this.vx = this.vx - dir.x;
 					this.vy = this.vy - dir.y;
